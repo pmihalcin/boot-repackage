8ec8dbbc626a6b7429a769d4053e59d42fac009b
mihalcin <patrik.mihalcin@embedit.cz>
2017-08-22 13:30:24 +0200

    Initial commit

---
+++ src/main/java/com/example/bootrepackage/BootRepackageApplication.java
@@ -0,0 +1,12 @@
+package com.example.bootrepackage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootRepackageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootRepackageApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/bootrepackage/BootRepackageApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.bootrepackage;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BootRepackageApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
